5f4c14bd6b9a6e68f47e832257ad0fa0cea32b9d
So <1979510177@qq.com>
2018-09-05 23:02:31 +0800

    add vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,25 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import state from './state'
 import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
+import createPersistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state,
-  mutations
+  mutations,
+  actions,
+  getters,
+  plugins: [
+    createPersistedState({
+      storage: {
+        paths: [], // An array of any paths to partially persist the state. If no paths are given, the complete state is persisted. (default: [])
+        getItem: key => wx.getStorageSync(key),
+        setItem: (key, value) => wx.setStorageSync(key, value),
+        removeItem: key => {}
+      }
+    })
+  ]
 })
