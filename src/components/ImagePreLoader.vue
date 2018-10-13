8f143bd1a68b9cf71b68efcfa26bb21297072f8b
So <1979510177@qq.com>
2018-10-13 17:41:41 +0800

    add image preloader

---
+++ src/components/ImagePreLoader.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <image v-for="(image,i) in images" :key="i" :src="image"  style="width:0;height:0;opacity:0" />
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      images: {
+        type: Array,
+        default: []
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
