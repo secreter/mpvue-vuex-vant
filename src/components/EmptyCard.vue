5bd55228e26673f513ae103b836ea23e5fb0802a
So <1979510177@qq.com>
2018-10-24 21:57:16 +0800

    add emptycard

---
+++ src/components/EmptyCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="empty-card">
+        <image class="empty" :src="image"/>
+        <div class="text">{{text}}</div>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      image: {
+        type: String,
+        default: '/static/img/no_orders.png'
+      },
+      text: {
+        type: String,
+        default: '暂无相关信息!'
+      }
+    }
+  }
+</script>
+
+<style lang="less">
+    .empty-card{
+        width: 100%;
+        background: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+        .empty{
+            width: 400rpx;
+            height: 300rpx;
+        }
+        .text{
+            color: #aaa;
+            font-size: 24rpx;
+            text-align: center;
+            padding-bottom: 30rpx;
+        }
+    }
+</style>
